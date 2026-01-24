a6c5a7c4b917a2400f138eb579514670e7baa177
J-c0d3r <juan.ferrugini@ges.inatel.br>
2026-01-24 15:19:31 -0300

    Functions.js code has been improved. Added more tests to GlobalSQA, totaling 15 tests in it. RegistrationLogin code has been improved

--- Cypress_Content/cypress/e2e/GlobalSQA/RegistrationLogin.cy.js
+++ Cypress_Content/cypress/e2e/GlobalSQA/RegistrationLogin.cy.js
@@ -1,22 +1,138 @@
 /// <reference types="cypress"/>
 
-//https://www.globalsqa.com/angularjs-protractor-practice-site/
+import { createUser } from '../../support/functions.js'
 
-describe('Criando cenário de teste para o site globalsqa', () => {
+/*https://www.globalsqa.com/angularjs-protractor-practice-site/*/
 
-  it('Caso de teste: Registrando um usuário no site com sucesso', () => {
-    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login');
+var user;
+
+function registerUser() {
+  user = createUser();
+  cy.get('#firstName').type(user[0]);
+  cy.get('#Text1').type(user[0]);
+  cy.get('#username').type(user[0]);
+  cy.get('#password').type(user[1]);
+  cy.get('.btn-primary').click();
+}
+
+describe('Testing GLOBALSQA - Register Login Example - Functionalities', () => {
+
+  beforeEach(() => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/');
+  });
+
+  it('Verificando Botão Cancel retornar para página Login', () => {
+    cy.get('.btn-link').click()
+    cy.get('.btn-link').click()
+    cy.get('h2').should('contain.text', "Login")
+  });
+
+  it('Registrando um usuário com sucesso', () => {
     cy.get('.btn-link').click();
-    cy.get('#firstName').type('inatel');
-    cy.get('#Text1').type('inatel');
-    cy.get('#username').type('inatel');
-    cy.get('#password').type('inatel');
-    cy.get('.btn-primary').click();
+    registerUser();
     cy.get('.ng-binding').should('contain.text', 'Registration successful');
   })
 
 
-  it('Caso de teste: Registrando um usuário com falha (faltando senha)', () => {
+  it('Registrando um usuário e Logando ambos com sucesso', () => {
+    cy.get('.btn-link').click();
+    registerUser();
+    cy.get('.ng-binding').should('contain.text', 'Registration successful');
+    cy.get('#username').type(user[0])
+    cy.get('#password').type(user[1])
+    cy.get('.btn-primary').click()
+    cy.get('h1.ng-binding').should('contain.text', "Hi " + user[0] + "!")
+    cy.get('div.ng-scope > :nth-child(2)').should('contain.text', "You're logged in!!")
+  })
+
+  it('Deletando usuário com sucesso!', () => {
+    cy.get('.btn-link').click();
+    registerUser();
+    cy.get('.ng-binding').should('contain.text', 'Registration successful');
+    cy.get('#username').type(user[0])
+    cy.get('#password').type(user[1])
+    cy.get('.btn-primary').click()
+    cy.get('div.ng-scope > :nth-child(2)').should('contain.text', "You're logged in!!")
+    cy.get('.ng-binding > a').click();
+    cy.get('.btn').click();
+    cy.get('#username').type(user[0])
+    cy.get('#password').type(user[1])
+    cy.get('.btn-primary').click()
+    cy.get('.ng-binding').should('have.text', 'Username or password is incorrect');
+  })
+
+  it('Registrando um usuário como todos os campos em branco', () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/register');
+    cy.get('.btn-primary').should('be.disabled');
+  })
+
+  it('Registrando um usuário Campo First Name em Branco', () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/register');
+    cy.get('#Text1').type('inatel');
+    cy.get('#username').type('inatel');
+    cy.get('#password').type('inatel');
+    cy.get('.btn-primary').should('be.disabled');
+  })
+
+  it('Registrando um usuário Campo Last Name em Branco', () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/register');
+    cy.get('#firstName').type('inatel');
+    cy.get('#username').type('inatel');
+    cy.get('#password').type('inatel');
+    cy.get('.btn-primary').should('be.disabled');
+  })
+
+  it('Registrando um usuário Campo Username em Branco', () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/register');
+    cy.get('#firstName').type('inatel');
+    cy.get('#Text1').type('inatel');
+    cy.get('#password').type('inatel');
+    cy.get('.btn-primary').should('be.disabled');
+  })
+
+  it('Registrando um usuário Campo Senha em Branco', () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/register');
+    cy.get('#firstName').type('inatel');
+    cy.get('#Text1').type('inatel');
+    cy.get('#username').type('inatel');
+    cy.get('.btn-primary').should('be.disabled');
+  })
+
+
+  it('Registrando um usuário. Campo First name Escreve, Apaga, Checa Error', () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/register');
+    cy.get('#firstName').type('inatel');
+    cy.get('#firstName').clear()
+    cy.get('#Text1').type('inatel');
+    cy.get('#username').type('inatel');
+    cy.get('#password').type('inatel');
+    cy.get('.has-error > .help-block').should('have.text', 'First name is required');
+    cy.get('.btn-primary').should('be.disabled');
+  })
+
+  it('Registrando um usuário. Campo Last name Escreve, Apaga, Checa Error', () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/register');
+    cy.get('#firstName').type('inatel');
+    cy.get('#Text1').type('inatel');
+    cy.get('#Text1').clear()
+    cy.get('#username').type('inatel');
+    cy.get('#password').type('inatel');
+    cy.get('.has-error > .help-block').should('have.text', 'Last name is required');
+    cy.get('.btn-primary').should('be.disabled');
+  })
+
+  it('Registrando um usuário. Username Escreve, Apaga, Checa Error', () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/register');
+    cy.get('#firstName').type('inatel');
+    cy.get('#Text1').type('inatel');
+    cy.get('#username').type('inatel');
+    cy.get('#username').clear()
+    cy.get('#password').type('inatel');
+    cy.get('.has-error > .help-block').should('have.text', 'Username is required');
+    cy.get('.btn-primary').should('be.disabled');
+  })
+
+  it('Registrando um usuário. Campo Senha Escreve, Apaga, Checa Error', () => {
     cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/register');
     cy.get('#firstName').type('inatel');
     cy.get('#Text1').type('inatel');
@@ -25,47 +141,44 @@ describe('Criando cenário de teste para o site globalsqa', () => {
     cy.get('#password').clear();
     cy.get('.has-error > .help-block').should('have.text', 'Password is required');
     cy.get('.btn-primary').should('be.disabled');
-    //cy.get('.btn-primary').click();
-  })
-
-
-  it('Caso de teste: Realizando login com sucesso', () => {
-    let info = criarUsuario();
-    cy.get('#username').type(info[0]);
-    cy.get('#password').type(info[1]);
-    cy.get('.btn-primary').click();
-    cy.get('h1.ng-binding').should('contain.text', info[0]);
-  })
-
-
-  it('Caso de teste: Deletando usuário com sucesso!', () => {
-
-    let info = criarUsuario();
-    cy.login(info[0], info[1]);
-    cy.get('.ng-binding > a').click();
-    cy.get('.btn').click();
-    cy.login(info[0], info[1]);
-    cy.get('.ng-binding').should('have.text', 'Username or password is incorrect');
   })
 })
 
 
-function criarUsuario() {
-  let horas = new Date().getHours().toString();
-  let minutos = new Date().getMinutes().toString();
-  let seg = new Date().getSeconds().toString();
-  let user = horas + minutos + seg + 'Id';
-  let senha = horas + minutos + seg + 'senha';
-  let userInfo = [user, senha];
+describe('Testing GLOBALSQA - Register Login Example - Visual Elements', () => {
+  it('Checando existência dos elementos na página login', () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/');
 
-  cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login');
-  cy.get('.btn-link').click();
-  cy.get('#firstName').type(user);
-  cy.get('#Text1').type(user);
-  cy.get('#username').type(user);
-  cy.get('#password').type(senha);
-  cy.get('.btn-primary').click();
-  cy.get('.ng-binding').should('contain.text', 'Registration successful');
+    cy.get('h2').should('exist')
+    cy.get('h2').should('contain.text', "Login")
 
-  return userInfo;
-}
+    cy.get('#username').should('exist')
+    cy.get('#password').should('exist')
+
+    cy.get('.credits > :nth-child(1)').should('exist')
+    cy.get('.credits > :nth-child(1)').should('contain.text', "AngularJS User Registration and Login Example")
+
+    cy.get(':nth-child(2) > a').should('exist')
+
+  });
+
+  it('Checando existência dos elementos na página register', () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/register');
+
+    cy.get('h2').should('exist')
+    cy.get('h2').should('contain.text', "Register")
+
+    cy.get('#firstName').should('exist')
+    cy.get('#Text1').should('exist')
+
+    cy.get('#username').should('exist')
+    cy.get('#password').should('exist')
+
+    cy.get('.credits > :nth-child(1)').should('exist')
+    cy.get('.credits > :nth-child(1)').should('contain.text', "AngularJS User Registration and Login Example")
+
+    cy.get(':nth-child(2) > a').should('exist')
+
+  });
+
+});
--- Cypress_Content/cypress/support/functions.js
+++ Cypress_Content/cypress/support/functions.js
@@ -1,14 +1,12 @@
-//trying to decouple the function used in api_users to here
+export function createUser(){
 
-function criarUsuario(){
+    let hours = new Date().getHours().toString();
+    let minutes = new Date().getMinutes().toString();
+    let sec = new Date().getSeconds().toString();
 
-    let hours = new Date().getHours.toString();
-    let minutes = new Date().getMinutes.toString();
-    let sec = new Date().getSeconds.toString();
-
-    let user = 'testID' + hour + minutes + sec;
-    let password = 'testPass';
-    let email = 'test' + hour + minutes + sec + '@qa.com.br';
+    let user = "testID" + hours + minutes + sec;
+    let password = 'testPassword';
+    let email = 'test' + hours + minutes + sec + '@qa.com.br';
 
     let userInfo = [user, password, email];
 
