8fca92f5b71f76f6417e9a1cd818fe96fa505816
J-c0d3r <juan.ferrugini@ges.inatel.br>
2026-01-24 17:11:09 -0300

    Added 13 tests to footer Inatel page

--- Cypress_Content/cypress/e2e/Inatel/home.cy.js
+++ Cypress_Content/cypress/e2e/Inatel/home.cy.js
@@ -47,7 +47,7 @@ describe('Testando primeiro layer da página', () => {
         cy.get('.SJ-MT-00 > .subtopic > h2').should('have.text', 'Visão')
     });
 
-    it('Verificando o funcionamento do botão Notícias', () => {        
+    it('Verificando o funcionamento do botão Notícias', () => {
         cy.get('#noticias-inatel').click()
         cy.get('summary').should('contain.text', 'Editorias')
         cy.get('h1').should('have.text', 'Todas')
@@ -98,6 +98,106 @@ describe('Testando segundo layer da página', () => {
     });
 });
 
+describe.only('Rodape', () => {
+
+    beforeEach(() => {
+        cy.visit("https://inatel.br")
+        cy.get('[id*="cookie"], [class*="cookie"]').invoke('remove')
+        cy.scrollTo("bottom")
+    });
+
+    it('Informações de endereço e contato', () => {
+        cy.get('.logoRodape > h2').should('exist')
+        cy.get('.mt15-1920').should('contain.text', "Instituto Nacional de Telecomunicações – Inatel")
+        cy.get('.logoRodape > :nth-child(3)').should('contain.text', "Campus em Santa Rita do Sapucaí - MG - Brasil")
+        cy.get('.logoRodape > :nth-child(4)').should('contain.text', "Av. João de Camargo, 510 - Centro - 37536-001")
+        cy.get('.logoRodape > :nth-child(5)').should('contain.text', "Telefone")
+        /* cy.get('.logoRodape > :nth-child(5) > a').should('contain.text', "+55 (35) 3471-9200") */
+        cy.get('.logoRodape > :nth-child(6)').should('contain.text', "WhatsApp")
+    });
+
+    it('Social Medias - Instagram', () => {
+        /* cy.get('[id*="cookie"], [class*="cookie"]').invoke('remove') */
+        /* cy.get('.redes-footer > ul > :nth-child(2) > a').scrollIntoView().click() */
+        cy.get('.redes-footer > ul > :nth-child(2) > a')
+            .should('be.visible')
+            .and('have.attr', 'href', 'https://www.instagram.com/inatel.tecnologias')
+            .and('have.attr', 'target', '_blank')
+    });
+
+    it('Social Medias - Youtube', () => {
+        cy.get('.redes-footer > ul > :nth-child(3) > a')
+            .should('be.visible')
+            .and('have.attr', 'href', 'https://www.youtube.com/channel/UCBB5HfmeAByTUs0XuzFXRew')
+            .and('have.attr', 'target', '_blank')
+    });
+
+    it('Social Medias - LinkedIN', () => {
+        cy.get('.redes-footer > ul > :nth-child(4) > a')
+            .should('be.visible')
+            .and('have.attr', 'href', 'https://www.linkedin.com/school/inatel/')
+            .and('have.attr', 'target', '_blank')
+    });
+
+    it('Social Medias - Flickr', () => {
+        cy.get('.redes-footer > ul > :nth-child(5) > a')
+            .should('be.visible')
+            .and('have.attr', 'href', 'https://www.flickr.com/photos/inatel/albums')
+            .and('have.attr', 'target', '_blank')
+    });
+
+    it('QR-Code e-MEC', () => {
+        cy.get('.infosRodape > a > picture > img').should('exist')
+    });
+
+
+    it('Links Gerais - Ouvidoria', () => {
+        cy.get('.mb30-1920 > :nth-child(1) > a').should('be.visible').click()
+        cy.url().should('include', '/ouvidoria')
+        cy.get('.flexBoxGeral').should('be.visible').should('contain.text', "Ouvidoria do Inatel")
+    });
+
+    it('Links Gerais - Carreiras', () => {
+        cy.get('.mb30-1920 > :nth-child(2) > a').should('be.visible').click()
+        cy.url().should('include', '/carreiras')
+        cy.get('h1.colorfff').should('be.visible').should('contain.text', "carreira")
+    });
+
+    it('Links Gerais - Política de Privacidade', () => {
+        cy.get('.mb30-1920 > :nth-child(4) > a')
+            .should('be.visible')
+            .and('have.attr', 'href', '/politica-privacidade')
+            .and('have.attr', 'target', '_blank')
+
+        /* cy.url().should('include', '/politica-privacidade')
+        cy.get('.flexBoxGeral').should('be.visible').should('contain.text', "Política de Privacidade") */
+    });
+
+    it('Links Gerais - Documentos Institucionais', () => {
+        cy.get('.mb30-1920 > :nth-child(5) > a').should('be.visible').click()
+        cy.url().should('include', '/documentos-institucionais')
+        cy.get('.flexBoxGeral').should('be.visible').should('contain.text', "Documentos Institucionais")
+    });
+
+    it('Links Gerais - Faça um Tour Virtual', () => {
+        cy.get('.mb30-1920 > :nth-child(6) > a').should('be.visible').click()
+        cy.url().should('include', '/conheca-o-inatel/')
+        /* cy.get('.flexBoxGeral').should('be.visible').should('contain.text', "Bem-vindo ao Inatel") */
+    });
+
+    it('Links Gerais - Notícias', () => {
+        cy.get('.mb30-1920 > :nth-child(7) > a').should('be.visible').click()
+        cy.url().should('include', '/noticias/')
+        cy.get('.flexBoxGeral').should('be.visible').should('contain.text', "Destaque")
+    });
+
+    it('Links Gerais - Mapa do Site', () => {
+        cy.get('.mb30-1920 > :nth-child(8) > a').should('be.visible').click()
+        cy.url().should('include', '/home/mapa-do-site')
+        cy.get('.colorfff').should('be.visible').should('contain.text', "Mapa do Site")
+    });
+});
+
 describe('Testando resoluções diferentes', () => {
     beforeEach(() => {
         cy.visit("https://inatel.br")
@@ -105,7 +205,7 @@ describe('Testando resoluções diferentes', () => {
     });
 
     it('Resolução 1920x1080', () => {
-        cy.viewport(1920,1080)
+        cy.viewport(1920, 1080)
         /* cy.get('.ma-navMobile.mobileHome').should('be.visible') */
         /* cy.get('.ui_w').should('be.visible') */
 
@@ -120,7 +220,7 @@ describe('Testando resoluções diferentes', () => {
     });
 
     it('Resolução iphone-6', () => {
-        cy.viewport('iphone-6')        
+        cy.viewport('iphone-6')
         cy.get('.ma-navMobile.mobileHome').should('be.visible')
         /* cy.get('.ui_w').should('be.visible') */
 
@@ -135,7 +235,7 @@ describe('Testando resoluções diferentes', () => {
     });
 
     it('Resolução 800x600', () => {
-        cy.viewport(800,600)
+        cy.viewport(800, 600)
         cy.get('.ma-navMobile.mobileHome').should('be.visible')
         /* cy.get('.ui_w').should('be.visible') */
 
