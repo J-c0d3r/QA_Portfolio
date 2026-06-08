fcc1f63176fc01b0b402df0eff36157225096823
J-c0d3r <juan.ferrugini@ges.inatel.br>
2026-06-08 18:42:39 -0300

    initializing in playwright with first tests

---
+++ Playwright/tests/saucedemo.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test'
+/* const {test, expect} = require('@playwright/test') */
+
+test('teste de login com sucesso', async ({ page }) => {
+
+    await page.goto('https://www.saucedemo.com/')
+    await page.locator('[data-test="username"]').fill('standard_user')
+    await page.locator('[data-test="password"]').fill('secret_sauce')
+    await page.locator('[id="login-button"]').click()
+
+    await expect(page).toHaveTitle(/Swag Labs/)
+
+})
